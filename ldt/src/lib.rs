575e0ee8607d100c1a7339bd65ed76d6ec1fb930
rjeli <eli@rje.li>
2023-11-09 15:11:35 -0800

    fixed bugs

--- ldt/src/lib.rs
+++ ldt/src/lib.rs
@@ -13,7 +13,8 @@ pub use ldt_based_pcs::*;
 use p3_challenger::FieldChallenger;
 use p3_commit::Mmcs;
 use p3_field::{
-    batch_multiplicative_inverse, extension::HasFrobenuis, AbstractField, ExtensionField, Field,
+    add_vecs, batch_multiplicative_inverse, cyclic_subgroup_coset_known_order,
+    extension::HasFrobenuis, scale_vec, sum_vecs, AbstractField, ExtensionField, Field,
     PackedField, TwoAdicField,
 };
 use p3_matrix::{
@@ -22,6 +23,7 @@ use p3_matrix::{
 };
 use p3_util::log2_strict_usize;
 pub use quotient::*;
+use tracing::{debug_span, instrument};
 
 extern crate alloc;
 
@@ -68,39 +70,57 @@ fn galois<F: Field, EF: HasFrobenuis<F>>(x: EF) -> [EF; 4] {
     xs
 }
 
-// idk naive lagrange? i got this from gpt4...
-fn lagrange<F: Field, const D: usize>(xs: [F; D], ys: [F; D]) -> [F; D] {
-    let mut poly = [F::zero(); D];
-
-    let mut den = [F::one(); D];
-    for i in 0..D {
-        for j in 0..D {
-            if i != j {
-                den[i] *= xs[i] - xs[j];
-            }
+// expands a product of binomials (x - xs[0])(x - xs[1]).. into coeffs
+fn binomial_expand<F: Field>(xs: &[F]) -> Vec<F> {
+    let mut coeffs = vec![F::zero(); xs.len() + 1];
+    coeffs[0] = F::one();
+    for (i, &x) in xs.iter().enumerate() {
+        for j in (1..i + 2).rev() {
+            coeffs[j] = coeffs[j - 1] - x * coeffs[j];
         }
+        coeffs[0] *= -x;
     }
-    batch_multiplicative_inverse(&mut den);
+    coeffs
+}
 
-    // welcome to malloc city
-    for i in 0..D {
-        let mut term_poly = vec![ys[i] * den[i]];
-        for j in 0..D {
-            if j != i {
-                let mut new_term_poly = vec![F::zero(); term_poly.len() + 1];
-                for k in 0..term_poly.len() {
-                    new_term_poly[k] -= xs[j] * term_poly[k];
-                    new_term_poly[k + 1] += term_poly[k];
-                }
-                term_poly = new_term_poly;
-            }
+fn galois_lagrange_basis<F: Field, EF: HasFrobenuis<F>>(alpha: EF) -> Vec<EF> {
+    let xs = galois(alpha);
+    let w_alpha = xs[1..]
+        .iter()
+        .map(|&xi| xs[0] - xi)
+        .product::<EF>()
+        .inverse();
+    binomial_expand(&xs[1..])
+        .into_iter()
+        .map(|c| c * w_alpha)
+        .collect()
+}
+
+#[instrument(skip_all, level = "debug")]
+fn remainder_polys<F: Field, EF: HasFrobenuis<F>>(alpha: EF, values: &[EF]) -> Vec<[F; 4]> {
+    let mut rs: Vec<[F; 4]> = vec![];
+    // precompute basis for alpha, Frob alpha, Frob^2 alpha, ..
+    let l_alpha = galois_lagrange_basis(alpha);
+    assert_eq!(l_alpha.len(), EF::D);
+    for &p_alpha in values {
+        let mut l_alpha_frob = scale_vec(p_alpha, l_alpha.clone());
+        let mut r = l_alpha_frob.clone();
+        for _ in 1..EF::D {
+            l_alpha_frob.iter_mut().for_each(|c| *c = c.frobenius());
+            r = add_vecs(r, l_alpha_frob.clone());
         }
-        for j in 0..term_poly.len() {
-            poly[j] += term_poly[j];
-        }
+        rs.push(
+            r.into_iter()
+                .map(|c| {
+                    assert!(<EF as ExtensionField<F>>::is_in_basefield(&c));
+                    c.as_base_slice()[0]
+                })
+                .collect_vec()
+                .try_into()
+                .unwrap(),
+        );
     }
-
-    poly
+    rs
 }
 
 fn polymul<F: Field>(p1: &[F], p2: &[F]) -> Vec<F> {
@@ -113,6 +133,7 @@ fn polymul<F: Field>(p1: &[F], p2: &[F]) -> Vec<F> {
     res
 }
 
+#[instrument(skip_all, level = "debug")]
 fn minpoly<F: Field, EF: HasFrobenuis<F>>(mut alpha: EF) -> Vec<F> {
     let mut poly = vec![EF::one()];
     for _ in 0..EF::D {
@@ -133,11 +154,13 @@ fn minpoly<F: Field, EF: HasFrobenuis<F>>(mut alpha: EF) -> Vec<F> {
 }
 
 // returns (poly - r) / m
+#[instrument(skip_all, level = "debug")]
 pub fn minpoly_quotient<F, EF, M>(
     inner: &M,
     data: &M::ProverData,
     alpha: EF,
     values: &[EF],
+    shift: F,
 ) -> RowMajorMatrix<F>
 where
     F: Field + TwoAdicField,
@@ -148,62 +171,167 @@ where
     let height = mat.height();
     let log_height = log2_strict_usize(height);
 
-    let alpha_frobs = galois(alpha);
-
     // calculate r for each poly
-    let mut rs = vec![];
-    for &val in values {
-        let p_a_frobs = galois(val);
-        let r: [F; 4] = lagrange(alpha_frobs, p_a_frobs).map(|coeff| {
-            assert!(<EF as ExtensionField<F>>::is_in_basefield(&coeff));
-            coeff.as_base_slice()[0]
-        });
-        rs.push(r);
-    }
+    let rs = remainder_polys(alpha, values);
+
+    let xs = cyclic_subgroup_coset_known_order(F::two_adic_generator(log_height), shift, height)
+        .collect_vec();
 
     // calculate m
-    let m = minpoly(alpha);
-    let xs = F::two_adic_generator(log_height)
-        .shifted_powers(F::generator())
-        .take(height);
-    let mut m_invs: Vec<F> = xs
-        .map(|x| m.iter().zip(x.powers()).map(|(a, b)| *a * b).sum())
-        .collect();
-    batch_multiplicative_inverse(&mut m_invs);
+    let m_invs = debug_span!("m_invs").in_scope(|| {
+        let m = minpoly(alpha);
+        batch_multiplicative_inverse(
+            &xs.iter()
+                .map(|&x| m.iter().zip(x.powers()).map(|(a, b)| *a * b).sum())
+                .collect_vec(),
+        )
+    });
 
     let mut qp = RowMajorMatrix::new(vec![F::zero(); mat.width() * mat.height()], mat.width());
-    let xs = F::two_adic_generator(log_height).shifted_powers(F::generator());
-    for (row, qp_row, x, m_inv) in izip!(mat.rows(), qp.rows_mut(), xs, m_invs) {
-        let x_pows: [F; 4] = (0..4)
-            .map(|i| x.exp_u64(i))
-            .collect_vec()
-            .try_into()
-            .unwrap();
-        for (y, qp_y, r) in izip!(row, qp_row, &rs) {
-            *qp_y = (*y - F::dot_product(&x_pows, &r)) * m_inv;
+    debug_span!("fill qp").in_scope(|| {
+        for (row, qp_row, x, m_inv) in izip!(mat.rows(), qp.rows_mut(), xs, m_invs) {
+            let x_pows: [F; 4] = (0..4)
+                .map(|i| x.exp_u64(i))
+                .collect_vec()
+                .try_into()
+                .unwrap();
+            for (y, qp_y, r) in izip!(row, qp_row, &rs) {
+                *qp_y = (*y - F::dot_product(&x_pows, &r)) * m_inv;
+            }
         }
-    }
+    });
 
     qp
 }
 
 #[cfg(test)]
 mod tests {
+    use std::println;
+
     use p3_baby_bear::BabyBear;
+    use p3_commit::DirectMmcs;
+    use p3_dft::{Radix2Dit, TwoAdicSubgroupDft};
     use p3_field::extension::BinomialExtensionField;
+    use p3_field::AbstractExtensionField;
+    use p3_interpolation::{interpolate_coset, interpolate_subgroup};
+    use p3_matrix::{MatrixRowSlicesMut, MatrixRows};
+    use p3_mds::coset_mds::CosetMds;
+    use p3_merkle_tree::FieldMerkleTreeMmcs;
+    use p3_poseidon2::{DiffusionMatrixBabybear, Poseidon2};
+    use p3_symmetric::{PaddingFreeSponge, TruncatedPermutation};
     use rand::{thread_rng, Rng};
+    use tracing::info_span;
+    use tracing_forest::ForestLayer;
+    use tracing_subscriber::layer::SubscriberExt;
+    use tracing_subscriber::util::SubscriberInitExt;
+    use tracing_subscriber::{EnvFilter, Registry};
 
     use super::*;
 
+    type F = BabyBear;
+    type EF = BinomialExtensionField<F, 4>;
+
+    type Val = BabyBear;
+    type Challenge = BinomialExtensionField<BabyBear, 4>;
+    type MyMds = CosetMds<Val, 16>;
+    type Perm = Poseidon2<Val, MyMds, DiffusionMatrixBabybear, 16, 5>;
+    type MyHash = PaddingFreeSponge<Perm, 16, 8, 8>;
+    type MyCompress = TruncatedPermutation<Perm, 2, 8, 16>;
+    type ValMmcs = FieldMerkleTreeMmcs<<Val as Field>::Packing, MyHash, MyCompress, 8>;
+
     #[test]
     fn test_minpoly() {
-        type F = BabyBear;
-        type EF = BinomialExtensionField<F, 4>;
         for _ in 0..1000 {
             let alpha: EF = thread_rng().gen();
             let m = minpoly(alpha);
             let sum: EF = alpha.powers().zip(m).map(|(x, c)| x * c).sum();
             assert_eq!(sum, EF::zero());
         }
+
+        info_span!("bar").in_scope(|| println!("hi"));
+    }
+
+    #[test]
+    fn test_binomial_expand() {
+        // (x - 1)(x - 2) = x^2 - 3x + 2
+        assert_eq!(
+            binomial_expand(&[F::one(), F::two()]),
+            vec![F::two(), -F::from_canonical_usize(3), F::one()]
+        );
+    }
+
+    fn reduce<F: Field>(coeffs: &[F], x: F) -> F {
+        coeffs
+            .iter()
+            .zip(x.powers())
+            .map(|(c, x_pow)| *c * x_pow)
+            .sum()
+    }
+
+    #[test]
+    fn test_remainder_poly() {
+        let mut rng = thread_rng();
+        let log_height = 5;
+        let trace: RowMajorMatrix<Val> = RowMajorMatrix::rand(&mut rng, 1 << log_height, 10);
+        let point: EF = rng.gen();
+        let values = interpolate_subgroup(&trace, point);
+        let rs = remainder_polys(point, &values);
+        for (r, y) in rs.into_iter().zip(values) {
+            // r(alpha) == p(alpha)
+            assert_eq!(
+                reduce(
+                    &r.into_iter().map(|x| EF::from_base(x)).collect_vec(),
+                    point
+                ),
+                y
+            );
+        }
+    }
+
+    fn max_deg<F: TwoAdicField>(m: &RowMajorMatrix<F>) -> usize {
+        let coeffs = Radix2Dit.idft_batch(m.clone());
+        coeffs.height()
+            - (0..coeffs.height())
+                .rev()
+                .take_while(|&r| coeffs.row(r).all(|x| x.is_zero()))
+                .count()
+    }
+
+    #[test]
+    fn test_minpoly_quotient() {
+        Registry::default()
+            .with(EnvFilter::from_default_env())
+            .with(ForestLayer::default())
+            .init();
+
+        let mut rng = thread_rng();
+        let shift = Val::generator();
+
+        let mds = MyMds::default();
+        let perm = Perm::new_from_rng(8, 22, mds, DiffusionMatrixBabybear, &mut rng);
+        let hash = MyHash::new(perm.clone());
+        let compress = MyCompress::new(perm.clone());
+        let inner = ValMmcs::new(hash, compress);
+
+        let width = 1024;
+        let log_height = 14;
+
+        let trace: RowMajorMatrix<Val> = RowMajorMatrix::rand(&mut rng, 1 << log_height, width);
+        let lde = Radix2Dit.coset_lde_batch(trace, 1, shift);
+        assert_eq!(max_deg(&lde), 1 << log_height);
+
+        let (_comm, data) = inner.commit_matrix(lde.clone());
+        let point: Challenge = rng.gen();
+        let mut values = interpolate_coset(&lde, shift, point);
+
+        let qp = minpoly_quotient(&inner, &data, point, &values, shift);
+        // passes ldt when opening valid
+        assert!(max_deg(&qp) <= (1 << log_height) - 4);
+
+        // invalidate the opening
+        values[100] = rng.gen();
+        let qp = minpoly_quotient(&inner, &data, point, &values, shift);
+        // now degree should be too high
+        assert!(max_deg(&qp) > (1 << log_height) - 4);
     }
 }
